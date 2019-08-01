e89f5392fe560e855309a6f64503d322c96f2719
Dylan Corvidae <dylancorvidae@gmail.com>
2019-07-31 17:57:05 -0700

    added chart.js, results.css and .html

--- src/app.js
+++ src/app.js
@@ -10,7 +10,11 @@ const centerImg = document.getElementById('center-img');
 const rightImg = document.getElementById('right-img');
 
 
-const displayList = new DisplayList(productsList);
+let displayList = new DisplayList(productsList);
+let productSelected = [];
+let productDisplayed = [];
+let round = 0;
+
 
 //take productSelection1 put into my buttons
 
@@ -19,15 +23,22 @@ const productSelection1 = displayList.getThreeRandomProducts();
 //button left
 const item1 = productSelection1[0];
 leftButton.value = item1.id;
-leftImg.src = item1.src;
+leftImg.src = item1.image;
 leftImg.alt = item1.name;
 //button center
 const item2 = productSelection1[1];
 centerButton.value = item2.id;
-centerImg.src = item2.src;
+centerImg.src = item2.image;
 centerImg.alt = item2.name;
 //button right
 const item3 = productSelection1[2];
 rightButton.value = item3.id;
-rightImg.src = item3.src;
+rightImg.src = item3.image;
 rightImg.alt = item3.name;
+
+
+//User choice selection and Tally
+leftButton.addEventListener('click', () => {
+    const idSelected = event.currentTarget.value;
+    
+})
